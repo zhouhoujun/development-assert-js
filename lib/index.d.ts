e84919b2898c4bbbe5ccf30065ae1e5525c9d7b2
unknown <>
2017-03-22 19:05:25 +0800

    update core dependence

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,25 @@
 import { IDynamicTaskOption, IAsserts, IDynamicTasks } from 'development-core';
+/**
+ * js assert task optin.
+ *
+ * @export
+ * @interface IJsTaskOption
+ * @extends {IAsserts}
+ */
 export interface IJsTaskOption extends IAsserts {
+    /**
+     * babel 6 option.
+     *
+     * @type {*}
+     * @memberOf IJsTaskOption
+     */
     babelOption?: any;
+    /**
+     * sourceMaps path.
+     *
+     * @type {string}
+     * @memberOf IJsTaskOption
+     */
     sourceMaps?: string;
 }
 export declare class JsTasks implements IDynamicTasks {
